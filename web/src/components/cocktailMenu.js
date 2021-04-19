50d3f5979848869985b4169c6e7166fe4866f85e
Nelson Caudill <nelson.a.caudill@gmail.com>
2021-04-19 03:41:14 -0400

    succesfully add react-to-print and save pdf

--- web/src/components/cocktailMenu.js
+++ web/src/components/cocktailMenu.js
@@ -4,13 +4,13 @@ import styled from 'styled-components'
 import Header from './cocktailMenuHeader'
 import DrinkList from './cocktailMenuDrinks'
 
-const CocktailMenu = ({drinks}) => (
-  <LetterheadWrapper>
+const CocktailMenu = React.forwardRef( ({drinks}, ref) => (
+  <LetterheadWrapper ref={ref}>
     <Header />
     <DecorativeBox />
     <DrinkList drinks={drinks} />
   </LetterheadWrapper>
-)
+))
 
 const LetterheadWrapper = styled.div`
   width: 8.5in;
@@ -21,7 +21,6 @@ const LetterheadWrapper = styled.div`
   bottom: 0;
   left: 0;
   margin: auto;
-  border: 1px solid;
 `
 
 const DecorativeBox = styled.div`
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useRef } from "react";
 import { graphql } from "gatsby";
 import { mapEdgesToNodes } from "../lib/helpers";
+import { useReactToPrint } from 'react-to-print'
+
 
 import GraphQLErrorList from "../components/graphql-error-list";
 import SEO from "../components/seo";
@@ -66,8 +68,12 @@ export const query = graphql`
 
 const IndexPage = (props) => {
   const { data, errors } = props;
+  const menuEl = useRef()
 
-  console.log(data)
+  const handlePrint = useReactToPrint({
+    content: () => menuEl.current,
+  });
+
   if (errors) {
     return (
       <Layout>
@@ -86,8 +92,6 @@ const IndexPage = (props) => {
       'Missing "Site settings". Open the studio at http://localhost:3333 and add some content to "Site settings" and restart the development server.'
     );
   }
-
-  console.log('Drink nodes: ', drinkNodes)
   
   return (
     <Layout>
@@ -96,7 +100,8 @@ const IndexPage = (props) => {
         description={site.description}
         keywords={site.keywords}
       />
-      {drinkNodes && <CocktailMenu drinks={drinkNodes} />}
+      {drinkNodes && <CocktailMenu ref={menuEl} drinks={drinkNodes} />}
+      <button onClick={handlePrint}>Print Menu</button>
     </Layout>
   );
 };

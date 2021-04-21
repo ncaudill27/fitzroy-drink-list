f0080dce7483d07d2655e35211808be9ef06a023
Nelson Caudill <nelson.a.caudill@gmail.com>
2021-04-21 01:35:34 -0400

    restyle cocktailMenuHeader to match original closer

--- web/src/components/cocktailMenuHeader.js
+++ web/src/components/cocktailMenuHeader.js
@@ -12,19 +12,18 @@ const Wrapper = styled.header`
   position: relative;
   width: calc(4in - 16px);
   height: 1.5625in; /* 1 9/16" */
-  left: 0.375in; /* 3/8" */
-  top: 0.9375in; /* 15/16" */
-  background-color: hsl(0, 0%, 90%);
+  top: 0.6875in; /* 11/16" */
+  background-color: hsl(0, 1%, 72%);
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: flex-end;
+  text-transform: uppercase;
   padding: 0 8px;
 `
 
 const Title = styled.h1`
   text-align: center;
-  text-transform: uppercase;
   width: fit-content;
   margin: 0;
   font-size: 3.3rem;
@@ -38,8 +37,10 @@ const The = styled.span`
 const Subtitle = styled.h2`
   margin: 0;
   width: fit-content;
+  font-family: 'Open Sans', sans-serif;
+  font-weight: 300;
   font-size:  2rem;
-  text-transform: uppercase;
+  color: hsl(357, 74%, 28%);
 `
 
 export default Header;
